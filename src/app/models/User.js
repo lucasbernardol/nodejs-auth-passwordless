b92be08087f6c2c1954571cc16d47f34bc0c9fa4
lucasbernardoph <lucasbernardo.touch@gmail.com>
2024-06-03 17:14:26 -0300

    Added User model

---
+++ src/app/models/User.js
@@ -0,0 +1,53 @@
+import crypto from 'node:crypto';
+import mongoose, { Schema } from 'mongoose';
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: false,
+      default: null,
+    },
+
+    uuid: {
+      type: String,
+      required: true,
+      unique: true,
+      default: () => crypto.randomUUID(),
+      select: false,
+    },
+
+    username: {
+      type: String,
+      required: false,
+      unique: true,
+      default: null,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    activatedAt: {
+      type: Number,
+      required: false,
+      default: null,
+      // UniX
+    },
+
+    finishedAt: {
+      type: Number,
+      required: false,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model('User', schema);
